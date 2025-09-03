37c2bef3c980a51cd6ae33efe4c12f878cbd581c
Stanislas THOT <sthot@groupama-ge.fr>
2025-09-03 14:54:47 +0200

    Add EventId property to DomainEvent class

--- CleanArchitecture.Domain/Common/DomainEvent.cs
+++ CleanArchitecture.Domain/Common/DomainEvent.cs
@@ -2,5 +2,6 @@ namespace CleanArchitecture.Domain.Common;
 
 public abstract class DomainEvent : IDomainEvent
 {
+    public Guid EventId { get; } = Guid.NewGuid();
     public DateTime OccurredOn { get; protected set; } = DateTime.UtcNow;
 }
